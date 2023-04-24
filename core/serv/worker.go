3e1072c9209551cc05d48753b4b8cac8ecddd4d9
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-04-24 14:42:19 +0800

    feat: 添加日志

--- core/serv/worker.go
+++ core/serv/worker.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ichaly/yugong/core/data"
 	"github.com/ichaly/yugong/core/util"
 	"gorm.io/gorm"
+	"log"
 	"os"
 	"path"
 	"strconv"
@@ -30,6 +31,7 @@ func (my *Task) process(worker string) {
 	timestamp := strconv.FormatInt(my.v.UploadAt.UnixNano()/1e6, 10)
 
 	//标题文件
+	log.Println("生成标题文件:" + my.v.Vid)
 	titleFile := path.Join(my.w, id, fmt.Sprintf("t0-%s.txt", my.v.Vid))
 	err := util.WriteFile(strings.NewReader(my.v.Title), titleFile)
 	if err != nil {
@@ -37,6 +39,7 @@ func (my *Task) process(worker string) {
 	}
 
 	//索引文件
+	log.Println("生成索引文件:" + my.v.Vid)
 	txtFile := path.Join(my.w, id, fmt.Sprintf("%s.txt", id))
 	filepath := fmt.Sprintf("daren/2215630453359/zip/%s___%s.zip", timestamp, my.v.Vid)
 	content := []string{my.v.Aid, filepath, timestamp, my.v.Vid, my.v.Fid}
@@ -46,6 +49,7 @@ func (my *Task) process(worker string) {
 	}
 
 	//下载封面
+	log.Println("开始下载封面:" + my.v.Vid)
 	coverFile := path.Join(my.w, id, fmt.Sprintf("c1-%s.jpg", my.v.Vid))
 	go func() {
 		defer wg.Done()
@@ -57,6 +61,7 @@ func (my *Task) process(worker string) {
 	}()
 
 	//下载视频
+	log.Println("开始下载视频:" + my.v.Vid)
 	videoFile := path.Join(my.w, id, fmt.Sprintf("v2-%s.mp4", my.v.Vid))
 	go func() {
 		defer wg.Done()
@@ -70,6 +75,7 @@ func (my *Task) process(worker string) {
 	wg.Wait()
 
 	//打包文件
+	log.Println("开始打包文件:" + my.v.Vid)
 	zipFile := path.Join(my.w, id, fmt.Sprintf("%s.zip", my.v.Vid))
 	err = util.Compress(zipFile, titleFile, videoFile, coverFile)
 	if err != nil {
@@ -82,16 +88,19 @@ func (my *Task) process(worker string) {
 	_ = os.Remove(videoFile)
 
 	//上传文件
+	log.Println("开始上传文件:" + my.v.Vid)
 	err = util.UploadFile(zipFile, fmt.Sprintf("%s___", timestamp))
 	if err != nil {
 		return
 	}
 	//上传索引
+	log.Println("开始上传索引:" + my.v.Vid)
 	err = util.UploadFile(txtFile, "index/")
 	if err != nil {
 		return
 	}
 
 	//更新状态
+	log.Println("开始更新状态:" + my.v.Vid)
 	my.d.Model(my.v).Update("state", 1)
 }
