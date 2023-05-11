825544dd7fff47018d8268e2958472f829b437d5
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-05-11 23:16:23 +0800

    feat: 优化代码

--- core/serv/xiaohongshu/xiaohongshu.go
+++ core/serv/xiaohongshu/xiaohongshu.go
@@ -216,45 +216,9 @@ func (my XiaoHongShu) session() (string, error) {
 		return ses, nil
 	}
 	uri, _ := url.Parse("https://edith.xiaohongshu.com/api/sns/web/v1/login/activate")
-	a1 := "18805433dffvrl8b2hut9pmkcdwizrt067cr7g6i330000326172"
-	//data := map[string]string{}
-	//header := my.script.Header(a1, uri, data)
-	req := serv.NewFetch(my.config).UseProxy().
-		SetCookies(map[string]string{
-			"a1":                a1,
-			"xhsTrackerId":      "6b23733f-4cf5-41dd-9cca-49379fa85fdd",
-			"xhsTrackerId.sig":  "Q8epTi0Bb_ZS-p7wWnXqmNMTm2BL19btcdna5GDdlSM",
-			"webId":             "8f93932a0b9cd184743340efeb37f28e",
-			"gid":               "yYY824qqiJK4yYY824qqfWkKiihF1YDJx79jS3qU6TSfEFq83uF98W888qJKyWJ8dfSD202Y",
-			"gid.sign":          "t3eeEgSrn9qRxwSMjVtIpNI9cKs=",
-			"xsecappid":         "xhs-pc-web",
-			"web_session":       "040069b5511a2a147061ab2a69364bc558a1f3",
-			"websectiga":        "6169c1e84f393779a5f7de7303038f3b47a78e47be716e7bec57ccce17d45f99",
-			"sec_poison_id":     "eec1467b-f5c0-4155-9103-48ad9c266bf7",
-			"extra_exp_ids":     "yamcha_0327_exp,h5_1208_exp3,ques_clt2",
-			"extra_exp_ids.sig": "ETM51AFqVyLPOioG2x0qNaEzMLVwrEIN37uTpfkLqxc",
-			"webBuild":          "2.4.5",
-			"acw_tc":            "736474944edf09618c1a6293d17361df55ee01fad643d8c9eb0ebf95c5682002",
-		}).
-		SetHeaders(map[string]string{
-			"sec-ch-ua":          "\"Microsoft Edge\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\"",
-			"x-t":                "1683804221472",
-			"x-b3-traceid":       "96cb6f358ddec0d0",
-			"sec-ch-ua-mobile":   "?0",
-			"user-agent":         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35",
-			"accept":             "application/json, text/plain, */*",
-			"x-s-common":         "2UQAPsHCPUIjqArjwjHjNsQhPsHCH0rjNsQhPaHCH0P1PUhIHjIj2eHjwjQ+GnPW/MPjNsQhPUHCHdYiqUMIGUM78nHjNsQh+sHCH0H1+shMHjIj2eLjwjHlweWI+/cAP9zf8d8UJeYjPfYM4eSIJnT08o4k2dQFPeG7G7H78A8kPAPIPeZIPAH9P/qUHjIj2eGjw0r9wePhPecUP0rF+AHVHdW7H0ijnbSgg9pEadkYp9zMp/+y4LSxJ9Swprbk/r+t2fbg8opnaBl7nS+Q+DS187YQyg4knpYs4M+gLnSOyLiFGLY+4B+o/gzDPS8kanS7ynPUJBEjJbkVG9EwqBHU+BSOyLShanS7yn+oz0pjzASinD+Q+DSxyBTw2f+7/D4yJLRoafMyJL83/dkiJLRryBMyyDph/fMnyMDUp/p+zFSh/nk+PLELc/m+pFEk/D4pPMSxzfTypbDUnfk3PSko//mwpBl3npzpPFECyA+8pMkk/dkQPbSxy74+pFME/DziyFExLfM+zrbhngkz+rEryBTOpbrI/FzbPSkLafl+PDkTnnkd+pkLafT8pr83np4zPDMg//b+zbbhnDzyybSCafSyzF83/fkByDELc/m+pMrUnp4+2pSTafY+pFFInfMwyDELpgS+zFpCnSzwJLRgn/Q8yfVl//QyJLRrz/pyyfzx/D4z2DRLn/myzbSh/Lzz4FEgpfM8pMk3//Qb+LErLgY8prQx/nkz2pSx8Az+JLMC/D4+2rMgzfS8PSDUnp4bPbkL//z82f4CnfkyJrMTagk+2SQT/L4ayrEx8A+yzMLF/Fz+PDEC8BM+Jp8T/Sz82DMrp/+8ySLInp4z2pkL/g4wprDU/M4tJbSTzfk+pb8knnMQPpSxafSOzrFA/SzVyDMxJBSyzrQT/pzb2pSL87kOpMbE/pzQ2LETzfkwpMS7/pzd2LECGAbwpFEinpz3PrEx87Y8ySLUnfkmPDELnfSOzBlVnD4b4MST/fTyzrrAnnMp2DMrp/pwPDk3/SzayDMTzfSyyfVF/FzDyFEx874yzbDF/F+twaHVHdWhH0ija/PhqDYD87+xJ7mdag8Sq9zn494QcUT6aLpPJLQy+nLApd4G/B4BprShLA+jqg4bqD8S8gYDPBp3Jf+m2DMBnnEl4BYQyrkSL98+zrTM4bQQPFTAnnRUpFYc4r4UGSGILeSg8DSkN9pgGA8SngbF2pbmqbmQPA4Sy9MaPpbPtApQy/8A8BE6q9k6pepEqgzGqgb7ngQsqnRQ2sV3zFzkN7+n4BTQ2emA2op7q0zl4BSQy7Q7anD6q9T0GA+QcFlfa/+O8/mM4BIUPrzyqFIM8Lz/afpxpdqIanSS8gYn4FI3Loz6qdbFyo4ya9phn/Y6aL+aPLSbqS+dpd4sz7bFLDShznbSzBzSpdp7yDSePoPlqgz1anTIaFSizo+S8/+Annzc+f+p+g+8qgzCGLzD8n8Cq/bC8sRSpM4BLLSh+nprpdz1aLpS8n8c4eSUJbiE/9RnzFS989pkqgzMNMSd8gYxqnMQyFl7anSrLr4YJnkQ4fYTtFDhwrSk2dpspd4Vag8oz0zS4fprLA4SPgpFcFSe+7+rGAFRHjIj2eDjw0DF+0DMPeGE+aIj2erIH0iAw/zR",
-			"x-s":                "XYW_eyJzaWduU3ZuIjoiNTAiLCJzaWduVHlwZSI6IngxIiwiYXBwSWQiOiJ4aHMtcGMtd2ViIiwic2lnblZlcnNpb24iOiIxIiwicGF5bG9hZCI6IjhkNzcwNGZmOGJmZmFkNzhmODhmZjExNmVkY2U5MGIxMjM1NTA0MWNiNGU3YjFkZTY2Zjk2ZGM0NTlkYTU3Nzk3YWZiNzI0YjkwMWMyNDhkNjRmMDAxYzQ4NDhkOTQ0ODE2ZTJlM2JmYjg5ZTJkYTFkYWQ2MWM1MDQxZDZhYzJiZGFkNjFjNTA0MWQ2YWMyYmJhMWM0ZmNjNTUyMGEzZTNmOWY2Yjk1M2ZmODE5ZjdjNGQzOTY0ZDYxMDQwNWVmYWZkM2E5NDQxYTBjMjQyYjg4MmMzNGMxMWFiY2Y2YWE0ZTM4YzgzZjZlMmIzMzRmMGRhNjg3ZGU4ODM2NzdmMmVmNTYzMDU3YjU0YWQyZTMwNTI2OWJlYmFjMTViYmI1YjJiODM3NTljMjliZDBmMTEyYTgzOWQyMTIyNmFjNWYwMTgyNzc1NWNhYTk0NjgxYjU2ZjA2NTZiODllZGEwYmNkZDA3YmUzMDU5N2JkNTRjMmFiZjk4ODdkNjgwZDY4OCJ9",
-			"sec-ch-ua-platform": "\"macOS\"",
-			"origin":             "https://www.xiaohongshu.com",
-			"sec-fetch-site":     "same-site",
-			"sec-fetch-mode":     "cors",
-			"sec-fetch-dest":     "empty",
-			"referer":            "https://www.xiaohongshu.com/",
-			"accept-encoding":    "gzip, deflate, br",
-			"accept-language":    "zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7",
-		})
-	//SetParams(data).SetHeaders(header)
+	a1 := "187a433ad934uk8iu6eapr38z41whjm51j84it9ja30000145194"
+	header := my.script.Header(a1, uri, nil)
+	req := serv.NewFetch(my.config).UseProxy().SetHeaders(header).SetCookies(map[string]string{"a1": a1})
 	var body string
 	zlog.Debug("开始登陆:" + uri.String())
 	res, err := req.Json(uri.String())
