53e7c655f2ef969ff85217768c70a55af3ff98c4
shi.changliang <shi.changliang@zhaopin.com.cn>
2023-05-11 20:11:47 +0800

    test: 添加测试数据

---
+++ docs/xiaohongshu/sesstion.sh
@@ -0,0 +1,32 @@
+curl\
+  -H "Host: edith.xiaohongshu.com"\
+  -b "a1=18805433dffvrl8b2hut9pmkcdwizrt067cr7g6i330000326172"\
+  -b "xhsTrackerId=6b23733f-4cf5-41dd-9cca-49379fa85fdd"\
+  -b "xhsTrackerId.sig=Q8epTi0Bb_ZS-p7wWnXqmNMTm2BL19btcdna5GDdlSM"\
+  -b "webId=8f93932a0b9cd184743340efeb37f28e"\
+  -b "gid=yYY824qqiJK4yYY824qqfWkKiihF1YDJx79jS3qU6TSfEFq83uF98W888qJKyWJ8dfSD202Y"\
+  -b "gid.sign=t3eeEgSrn9qRxwSMjVtIpNI9cKs="\
+  -b "xsecappid=xhs-pc-web"\
+  -b "extra_exp_ids=yamcha_0327_exp,h5_1208_exp3,ques_clt2"\
+  -b "extra_exp_ids.sig=ETM51AFqVyLPOioG2x0qNaEzMLVwrEIN37uTpfkLqxc"\
+  -b "webBuild=2.4.5"\
+  -b "acw_tc=736474944edf09618c1a6293d17361df55ee01fad643d8c9eb0ebf95c5682002"\
+  -b "websectiga=8886be45f388a1ee7bf611a69f3e174cae48f1ea02c0f8ec3256031b8be9c7ee"\
+  -b "sec_poison_id=6de6a4f2-236e-4f00-ab9b-f01f4f82a4f2"\
+  -H "sec-ch-ua: \"Microsoft Edge\";v=\"113\", \"Chromium\";v=\"113\", \"Not-A.Brand\";v=\"24\""\
+  -H "x-t: 1683805511467"\
+  -H "x-b3-traceid: 98e23753400ab82f"\
+  -H "sec-ch-ua-mobile: ?0"\
+  -H "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.35"\
+  -H "content-type: application/json;charset=UTF-8"\
+  -H "accept: application/json, text/plain, */*"\
+  -H "x-s-common: 2UQAPsHCPUIjqArjwjHjNsQhPsHCH0rjNsQhPaHCH0P1PUhIHjIj2eHjwjQ+GnPW/MPjNsQhPUHCHdYiqUMIGUM78nHjNsQh+sHCH0H1+shMHjIj2eLjwjHlweWI+/cAP9zf8d8UJeYjPfYM4eSIJnT08o4k2dQFPeG7G7H78A8kPAPIPeZIPAH9P/qUHjIj2eGjw0r9wePhPeLMP/rF+0qVHdW7H0ijnbSgg9pEadkYp9zMp/+y4LSxJ9Swprbk/r+t2fbg8opnaBl7nS+Q+DS187YQyg4knpYs4M+gLnSOyLiFGLY+4B+o/gzDPS8kanS7ynPUJBEjJbkVG9EwqBHU+BSOyLShanS7yn+oz0pjzASinD+Q+DSxnfM+PDMC/p4z2rMgL/pwzB+h/SzmPSkxLfT+2f47nnknyMkL//p+PSDA/p4+2bkrnfk+yDQxn/QnyDRLpfTyzMkknnkaybDU//zypFDM/FzyJLRoagY+yS83/Szz2SSgz/m+pB+7/Lz++pSLpfMOpBli/0Q82bkxzfYwzF8xngkbPSkLafl+PDkTnnkd+pkLafT8pr83np4zPDMg//b+zbbhnDzyybSCafSyzF83/fkByDELc/m+pMrUnp4+2pSTafY+pFFInfMwyDELpgS+zFpCnSzwJLRgn/Q8yfVl//QyJLRrz/pyyfzx/D4z2DRLn/myzbSh/Lzz4FEgpfM8pMk3//Qb+LErLgY8prQx/nkz2pSx8Az+JLMC/D4+2rMgzfS8PSDUnp4bPbkL//z82f4CnfkyJrMTagk+2SQT/L4ayrEx8A+yzMLF/Fz+PDEC8BM+Jp8T/Sz82DMrp/+8ySLInp4z2pkL/g4wprDU/M4tJbSTzfk+pb8knnMQPpSxafSOzrFA/SzVyDMxJBSyzrQT/pzb2pSL87kOpMbE/pzQ2LETzfkwpMS7/pzd2LECGAbwpFEinpz3PrEx87Y8ySLUnfkmPDELnfSOzBlVnD4b4MST//z+ySDAnSz3PrMr8A++pMLlnDz++bSLzfTwpFEVnSzwyMkxn/QOzrk3/D+twaHVHdWhH0ija/PhqDYD87+xJ7mdag8Sq9zn494QcUT6aLpPJLQy+nLApd4G/B4BprShLA+jqg4bqD8S8gYDPBp3Jf+m2DMBnnEl4BYQyrkSzBE+zrTM4bQQPFTAnnRUpFYc4r4UGSGILeSg8DSkN9pgGA8SngbF2pbmqbmQPA4Sy9MaPpbPtApQy/8A8BE6q9k6pepEqgzGqgb7ngQsqnRQ2sV3zFzkN7+n4BTQ2emA2op7q0zl4BSQy7Q7anD6q9T0GA+QcFlfa/+O8/mM4BIUPrzyqFIM8Lz/afpxpdqIanSS8gYn4FI3Loz6qdbFybmT89p3JpqFagW3yLSiqSQCpd4daMmFnLDALpm6qDTS+dbFyrShN7P9pdz6anS+JFSkyBp1808SL9zl2oQf4d+x4gzNqSkd8n80q0Sd8DTSnn+cqFShN9pfpdzYaLpN8nzc4rSjnLzp4e4PJDSh+gPAqgzaJfP78p8fzUTQy9+7anSH4ecIGDMQ4fYl4bPh8FSkyn8Ipd4maL+HpDk6P9p/qBpSyMmFpDShP9LI8rRAp9PROaHVHdWEH0iTP/DF+ec9+0L7PaIj2erIH0iFPe+R"\
+  -H "x-s: XYW_eyJzaWduU3ZuIjoiNTAiLCJzaWduVHlwZSI6IngxIiwiYXBwSWQiOiJ4aHMtcGMtd2ViIiwic2lnblZlcnNpb24iOiIxIiwicGF5bG9hZCI6IjZmM2MzMWQxMWQ5NDcxNTA2ZjRkMzgwYjVkZTM5M2Y3MWMxZDZjMjBjY2VjOTVkZGZiYjRhY2M4ZWI5ODZmOGIxMjVkNTQzYWE0MTcwMDM5YTVmOTlhN2YxZjFhNGFjYzE2ZTJlM2JmYjg5ZTJkYTFkYWQ2MWM1MDQxZDZhYzJiZGFkNjFjNTA0MWQ2YWMyYmJhMWM0ZmNjNTUyMGEzZTNmOWY2Yjk1M2ZmODE5ZjdjNGQzOTY0ZDYxMDQwNWVmYWZkM2E5NDQxYTBjMjQyYjg4MmMzNGMxMWFiY2Y2YWE0ZTM4YzgzZjZlMmIzMzRmMGRhNjg3ZGU4ODM2NzdmMmVmNTYzMDU3YjU0YWQyZTMwNTI2OWJlYmFjMTViYmI1YjJiODM3NTljMjliZDBmMTEyYTgzOWQyMTIyNmFjNWYwMTgyNzc1NWNhYTk0NjgxYjU2ZjA2NTZiODllZGEwYmM4MjY3ZTk0MDg3MWU1ZDM4YTFkNWNlZTNkZjY2ODJkNCJ9"\
+  -H "sec-ch-ua-platform: \"macOS\""\
+  -H "origin: https://www.xiaohongshu.com"\
+  -H "sec-fetch-site: same-site"\
+  -H "sec-fetch-mode: cors"\
+  -H "sec-fetch-dest: empty"\
+  -H "referer: https://www.xiaohongshu.com/"\
+  -H "accept-language: zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7" \
+  --data-binary "{}" --compressed "https://edith.xiaohongshu.com/api/sns/web/v1/login/activate"\
